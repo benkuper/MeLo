83d2f7f433a3bf14effbd8ef26caad8669319757
Ben Kuper <bkuperberg@gmail.com>
2023-07-27 18:28:37 +0200

    ignore Export folder

--- Assets/Melo/Scripts/MeloManager.cs
+++ Assets/Melo/Scripts/MeloManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using OSCQuery.UnityOSC;
-using static UnityEditor.Progress;
 
 [System.Serializable]
 public class MeloType
